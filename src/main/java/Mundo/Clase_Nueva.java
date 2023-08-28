b9f110ea156681c17eaf6b5a8795b552a43fc0d8
Jefferson <jschafuelan@espe.edu.ec>
2023-08-28 14:21:37 -0500

    Clase de prueba agregada por jefferson

---
+++ src/main/java/Mundo/Clase_Nueva.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Mundo;
+
+/**
+ *
+ * @author Maicol Andres
+ */
+public class Clase_Nueva {
+    String nombres;
+    int num;
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+    
+    
+}
